a513bab442b0b3af2139416626d4311c8f041074
Kiria <kmilvat@mail.ru>
2020-06-17 10:50:45 +0700

    C

---
+++ src/calculator.c
@@ -0,0 +1,16 @@
+#include "calculator.h"
+
+
+
+float sum(float x, float y){
+    return x + y;
+}
+float sub(float x, float y){
+    return x - y;
+}
+float del(float x, float y){
+    return x / y;
+}
+float mul(float x, float y){
+    return x * y;
+}

b791c12791b60f474e2195f8cb5f0808c0484267
Kiria <kmilvat@mail.ru>
2020-06-17 11:58:03 +0700

    MAIN

--- src/main.c
+++ src/main.c
@@ -4,44 +4,41 @@
 // #include <cstdlib>
 // using namespace std;
 
-int main(int argc, char* argv[])
-{
-    // setlocale(LC_ALL,"russian");
-    char ss;
+int main(int argc, char *argv[]) {
+  // setlocale(LC_ALL,"russian");
+  char ss;
 
-    do {
-        printf("Введите арифметическое действие: число,знак(/,+,-,*),число\n");
-        float x, y, r;
-        char o;
+  do {
+    printf("Введите арифметическое действие: число,знак(/,+,-,*),число\n");
+    float x, y, r;
+    char o;
 
-        scanf("%f", &x);
-        scanf("%s", &o);
-        scanf("%f", &y);
+    scanf("%f", &x);
+    scanf("%s", &o);
+    scanf("%f", &y);
 
-        switch (o) {
-        case '+':
-            r = sum(x, y);
-            break;
+    switch (o) {
+    case '+':
+      r = sum(x, y);
+      break;
 
-        case '-':
-            r = sub(x, y);
-            break;
+    case '-':
+      r = sub(x, y);
+      break;
 
-        case '*':
-            r = mul(x, y);
-            break;
+    case '*':
+      r = mul(x, y);
+      break;
 
-        case '/':
-            r = del(x, y);
-            break;
-        }
-        printf("%f %c %f = %f\n", x, o, y, r);
+    case '/':
+      r = del(x, y);
+      break;
+    }
+    printf("%f %c %f = %f\n", x, o, y, r);
+    printf("Продолжить?(y/n)");
+    scanf("%s", &ss);
 
-        printf("Продолжить?(y/n)");
+  } while (ss == 'y');
 
-        scanf("%s", &ss);
-
-    } while (ss == 'y');
-
-    return 0;
+  return 0;
 }
